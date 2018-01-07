10c4d37c335ecd2f247246e06b70b2344b155e20
Mladen <djoml8bre@gmail.com>
2018-01-08 00:12:48 +0100

    Commit-7 Passing Parameters to Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,10 @@ const appRoutes: Routes = [
     component: UsersComponent
   },
   {
+    path: 'users/:id',
+    component: UserComponent
+  },
+  {
     path: 'servers',
     component: ServersComponent
   },
